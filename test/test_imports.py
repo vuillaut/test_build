cb1add57b74e62ad509c50400001b50d5b32b12d
vuillaut <thomas.vuillaume@gmail.com>
2020-01-28 18:55:07 +0100

    adding simple travis and test

---
+++ test/test_imports.py
@@ -0,0 +1,16 @@
+
+
+def test_import_ctapipe():
+    import ctapipe
+
+def test_import_lstchain():
+    import lstchain
+
+def test_import_protopipe():
+    import protopipe
+
+def test_import_protozfits():
+    import protozfits
+
+def test_import_ctapipe_io_lst():
+    import ctapipe_io_lst
